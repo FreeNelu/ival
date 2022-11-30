19248b560b3a6dd9cb62d99a129b9ec3fbf8e50f
FreeNelu <nelu.maciuca99@gmail.com>
2022-11-30 21:24:34 +0200

    Basic Auth

--- App.tsx
+++ App.tsx
@@ -3,14 +3,28 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
+import React from "react";
 import Navigation from "./navigation";
 import { Amplify } from "aws-amplify";
 import config from "./src/aws-exports";
-import { withAuthenticator } from "aws-amplify-react-native";
+import { signUpConfig } from "./src/signUpConfig";
+import { I18n } from "aws-amplify";
+
+import {
+  withAuthenticator,
+  useAuthenticator,
+} from "@aws-amplify/ui-react-native";
+import { Button } from "react-native";
 
 Amplify.configure(config);
+I18n.setLanguage("fr");
 
-export default function App() {
+const SignOutButton = () => {
+  const { signOut } = useAuthenticator();
+  return <Button onPress={signOut} title="Sign Out" />;
+};
+
+const App = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -19,9 +33,12 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+        <SignOutButton />
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
     );
   }
-}
+};
+
+export default withAuthenticator(App);
---
+++ src/signUpConfig.ts
@@ -0,0 +1,55 @@
+export const signUpConfig = {
+  hideAllDefaults: true,
+  defaultCountryCode: "33",
+  signUpFields: [
+    {
+      label: "Full name",
+      key: "name",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+    },
+    {
+      label: "Phone Number",
+      key: "phone_number",
+      required: true,
+      displayOrder: 3,
+      type: "string",
+    },
+    {
+      label: "Username",
+      key: "preferred_username",
+      required: true,
+      displayOrder: 4,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 5,
+      type: "password",
+    },
+    {
+      label: "Birthdate",
+      key: "birthdate",
+      required: true,
+      displayOrder: 6,
+      type: "default",
+    },
+    {
+      label: "Address",
+      key: "address",
+      required: true,
+      displayOrder: 7,
+      type: "default",
+    },
+  ],
+};
